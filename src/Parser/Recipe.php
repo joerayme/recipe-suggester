a1fd4926373e91f2b62f37ef336b73a85d4de838
Joe Ray <joeray123@googlemail.com>
2014-05-14 13:39:37 +1000

    Add Recipe JSON parser

--- src/Parser/Recipe.php
+++ src/Parser/Recipe.php
@@ -10,6 +10,58 @@ namespace RecipeSuggester\Parser;
  *
  * @author Joseph Ray <joe@joeray.me>
  */
-interface Recipe extends \RecipeSuggester\Parser
+abstract class Recipe implements \RecipeSuggester\Parser
 {
+    /**
+     * 
+     * @param \stdClass $object
+     * @return \RecipeSuggester\Model\Recipe
+     */
+    protected static function createRecipeFromDataObject(\stdClass $object)
+    {
+        if (!isset($object->name))
+        {
+            throw new InvalidFormatException('Invalid recipe list: recipe name was not provided');
+        }
+
+        if (!isset($object->ingredients) || !is_array($object->ingredients)
+            || empty($object->ingredients)
+        ) {
+            throw new InvalidFormatException('Invalid recipe list: no ingredients for recipe "' . $object->name . '"');
+        }
+
+        return new \RecipeSuggester\Model\Recipe(
+            $object->name,
+            self::createIngredientsFromDataObjects($object->name, $object->ingredients)
+        );
+    }
+
+    /**
+     *
+     * @param string $name
+     * @param array  $ingredients
+     * @return array
+     */
+    protected static function createIngredientsFromDataObjects($name, array $ingredients)
+    {
+        $return = array();
+
+        foreach ($ingredients as $ingredient)
+        {
+            if (!isset($ingredient->item, $ingredient->amount, $ingredient->unit)
+                || !is_numeric($ingredient->amount)
+                || !in_array($ingredient->unit, \RecipeSuggester\Model\Ingredient::$validUnits))
+            {
+                throw new InvalidFormatException('Invalid ingredients list for recipe "' . $name . '"');
+            }
+
+            $return[] = new \RecipeSuggester\Model\Ingredient(
+                $ingredient->item,
+                intval($ingredient->amount),
+                $ingredient->unit
+            );
+        }
+
+        return $return;
+    }
 }
--- src/Parser/Recipe/Json.php
+++ src/Parser/Recipe/Json.php
@@ -11,10 +11,32 @@ namespace RecipeSuggester\Parser\Recipe;
  *
  * @author Joseph Ray <joe@joeray.me>
  */
-class Json implements \RecipeSuggester\Parser\Recipe
+class Json extends \RecipeSuggester\Parser\Recipe
 {
+    /**
+     *
+     * @param string $string
+     */
     public function parse($string)
     {
+        $return  = array();
+        $recipes = json_decode($string, false);
 
+        if (!$recipes)
+        {
+            throw new \RecipeSuggester\Parser\InvalidFormatException('Invalid JSON in recipe list');
+        }
+
+        if (!is_array($recipes) || empty($recipes))
+        {
+            throw new \RecipeSuggester\Parser\InvalidFormatException('No recpies provided in JSON');
+        }
+
+        foreach ($recipes as $recipeRaw)
+        {
+            $return[] = self::createRecipeFromDataObject($recipeRaw);
+        }
+
+        return $return;
     }
 }
